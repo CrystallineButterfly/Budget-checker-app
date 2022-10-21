f6bdae213354cc6bcfb49390745c10a0801c9b89
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-10-21 21:56:08 +0100

    Add deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  resources :users
+  resources :catergories do
+    resources :spendings
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#splash"
 end
